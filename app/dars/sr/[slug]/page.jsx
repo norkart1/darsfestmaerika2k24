ed028fdfbdbf7299f3b400281edb495dbaa12acd
Capt Nork <138743746+norkart1@users.noreply.github.com>
2024-09-01 21:30:24 +0530

    Update page.jsx

--- app/dars/sr/[slug]/page.jsx
+++ app/dars/sr/[slug]/page.jsx
@@ -20,8 +20,8 @@ function DarsDetails() {
     values.some((value) => value && value.includes(number + 9));
   return (
     <div className="text-center text-xs m-10 print:m-0 p-8 rounded-xl mr-6">
-      <p className="text-3xl font-bold ">Jamia Dars Fest 2023-&apos;24</p>
-      <p className="text-2xl font-bold mb-2">Kondotty Zone</p>
+      <p className="text-3xl font-bold ">Maerika Art Fest 2k24 </p>
+      <p className="text-2xl font-bold mb-2">Jawharathul Uloom Suffa Dars Arts Fest</p>
       <div className="">
         <div className="flex h-48 items-end mb-8 w-[1260px]">
           <div className="w-[400px] my-auto text-center">
@@ -33,50 +33,26 @@ function DarsDetails() {
               <p className="w-6 -rotate-90">ഖിറാഅത്ത്</p>
               <p className="w-6 -rotate-90">ഹിഫ്ള്</p>
               <p className="w-6 -rotate-90">വാങ്ക്</p>
-              <p className="w-6 -rotate-90">ക്ലാസ് അവതരണം </p>
-              <p className="w-6 -rotate-90">വഅ്ള് </p>
-              <p className="w-6 -rotate-90">പ്രസംഗം മലയാളം </p>
               <p className="w-6 -rotate-90">പ്രസംഗം അറബി </p>
-              <p className="w-6 -rotate-90">പ്രസംഗം ഉറുദു </p>
-              <p className="w-6 -rotate-90">പ്രസംഗം ഇംഗ്ലീഷ് </p>
-              <p className="w-6 -rotate-90">ഖുഥ്ബ </p>
-              <p className="w-6 -rotate-90">ഗ്രന്ഥ വായന </p>
-              <p className="w-6 -rotate-90">കവിതാലാപനം മലയാളം </p>
-              <p className="w-6 -rotate-90">മുശാഅറ </p>
+              <p className="w-6 -rotate-90">പ്രസംഗം മലയാളം </p>
+              <p className="w-6 -rotate-90">പ്രസംഗം ഇംഗ്ലീഷ് </p>           
+              <p className="w-6 -rotate-90">അറബി ഗാനം</p>        
+              <p className="w-6 -rotate-90">മലയാള ഗാനം</p>
+              <p className="w-6 -rotate-90">ഉറുദു ഗാനം</p>
+              <p className="w-6 -rotate-90">മധുരമലയാളം</p>
+              <p className="w-6 -rotate-90">ക്ലാസ് അവതരണം </p>
               <p className="w-6 -rotate-90">അനൗണ്‍സ്മെന്‍റ് </p>
-              <p className="w-6 -rotate-90">പ്രബന്ധാവതരണം </p>
-              <p className="w-6 -rotate-90">നിമിഷപ്രസംഗം </p>
-              <p className="w-6 -rotate-90">പാടിപ്പറയല്‍ </p>
-              <p className="w-6 -rotate-90">
-                മാഷപ്പ് മാപ്പിളപ്പാട്ട്
-              </p>
-              <p className="w-6 -rotate-90">ഖസ്വീദ പാരായണം </p>
-              <p className="w-6 -rotate-90">പ്രോഗ്രാം സെറ്റിംഗ് </p>
-              <p className="w-6 -rotate-90">അറബിക് കാലിഗ്രഫി </p>
-              <p className="w-6 -rotate-90">പോസ്റ്റര്‍ ഡിസൈനിംഗ് </p>
-              <p className="w-6 -rotate-90">ഖത്തുന്നസ്ഖ് </p>
-              <p className="w-6 -rotate-90">ഖത്തുറുഖഈ </p>
-              <p className="w-6 -rotate-90">വിവര്‍ത്തനം (അറ-മല) </p>
-              <p className="w-6 -rotate-90">വിവര്‍ത്തനം (മല-അറ)</p>
-              <p className="w-6 -rotate-90">
-                വിവര്‍ത്തനം (അറ-ഇംഗ്ലി)
-              </p>
-              <p className="w-6 -rotate-90">പ്രബന്ധം അറബി </p>
-              <p className="w-6 -rotate-90">പ്രബന്ധം ഇംഗ്ലീഷ് </p>
-              <p className="w-6 -rotate-90">പ്രബന്ധം മലയാളം </p>
-              <p className="w-6 -rotate-90">റിപ്പോര്‍ട്ടിംഗ് </p>
-              <p className="w-6 -rotate-90">തലവാചക നിര്‍മ്മാണം </p>
-              <p className="w-6 -rotate-90">മുദ്രാവാക്യ രചന </p>
-              <p className="w-6 -rotate-90">കവിതാ രചന (അറ) </p>
-              <p className="w-6 -rotate-90">
-                നിഘണ്ടു നിര്‍മ്മാണം (അറ){" "}
-              </p>
-              <p className="w-6 -rotate-90">അടിക്കുറിപ്പ് </p>
-              <p className="w-6 -rotate-90">പദ സമ്പാദനം </p>
-              <p className="w-6 -rotate-90">തശ്കീല്‍ </p>
-              <p className="w-6 -rotate-90">പ്രശ്നോത്തരി </p>
-              <p className="w-6 -rotate-90">പത്രനിര്‍മാണം</p>
-            </div>
+              <p className="w-6 -rotate-90">വഅ്ള് </p>
+              <p className="w-6 -rotate-90">കവിതാലാപനം</p>
+              <p className="w-6 -rotate-90">മുശാഅറ </p>
+              <p className="w-6 -rotate-90">ആദർശ ക്ലാസ്സ്</p>
+              <p className="w-6 -rotate-90">മാലപ്പാട്ട്</p>
+              <p className="w-6 -rotate-90">സംഘഗാനം</p>
+              <p className="w-6 -rotate-90">കഥാ പ്രസംഗം</p>
+              <p className="w-6 -rotate-90">ബുറുദ</p>
+
+              
+             </div>
           </div>
         </div>
       </div>
