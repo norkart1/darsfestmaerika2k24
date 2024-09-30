ecfa1c4d0dd17dc1578825fba92b5780cf3d0d2e
Capt Nork <138743746+norkart1@users.noreply.github.com>
2024-09-30 13:43:29 +0530

    Update page.jsx

--- app/program/[slug]/page.jsx
+++ app/program/[slug]/page.jsx
@@ -1,4 +1,5 @@
-"use client";
+"use client"; // This tells Next.js to render this component on the client-side
+
 import React from "react";
 import { useParams } from "next/navigation";
 import Data from "../../../data/FullData.json";
@@ -7,92 +8,89 @@ function ProgramDetail() {
   const { slug } = useParams();
   const programFields = [
     "stage1",
-      "stage2",
-      "stage3",
-      "stage4",
-      "stage5",
-      "stage6",
-      "stage7",
-      "stage8",
-      "stage9",
-      "stage10",
-      "stage11",
-      "stage12",
-      "stage13",
-      "stage14",
-      "stage15",
-      "stage16",
-      "stage17",
-      "stage18",
-      "stage19",
-      "stage20",
-      "stage21",
-      "stage22",
-      "stage23",
-      "stage24",
-      "stage25",
-      "stage26",
-      "stage27",
-      "stage28",
-      "stage29",
-      "stage30",
-
+    "stage2",
+    "stage3",
+    "stage4",
+    "stage5",
+    "stage6",
+    "stage7",
+    "stage8",
+    "stage9",
+    "stage10",
+    "stage11",
+    "stage12",
+    "stage13",
+    "stage14",
+    "stage15",
+    "stage16",
+    "stage17",
+    "stage18",
+    "stage19",
+    "stage20",
+    "stage21",
+    "stage22",
+    "stage23",
+    "stage24",
+    "stage25",
+    "stage26",
+    "stage27",
+    "stage28",
+    "stage29",
+    "stage30",
     "offstage1",
-      "offstage2",
-      "offstage3",
-      "offstage4",
-      "offstage5",
-      "offstage6",
-      "offstage7",
-      "offstage8",
-      "offstage9",
-      "offstage10",
-      "offstage11",
-      "offstage12",
-      "offstage13",
-      "offstage14",
-      "offstage15",
-      "offstage16",
-      "offstage17",
-      "offstage18",
-      "offstage19",
-      "offstage20",
-      "offstage21",
-      "offstage22",
-      "offstage23",
-      "offstage24",
-      "offstage25",
-      "offstage26",
-      "offstage27",
-      "offstage28",
-      "offstage29",
-      "offstage30",
-      "offstage31",
-      "offstage32",
-      "offstage33",
-      "offstage34",
-      "offstage35",
-      "offstage36",
-      "offstage37",
-      "offstage38",
-      "offstage39",
-      "offstage40",
-      "offstage41",
-      "offstage42",
-      "offstage43",
-      "offstage44",
-      "offstage45",
-      "offstage46",
-      "offstage47",
-      "offstage48",
-      "offstage49",
-      "offstage50",
-    
+    "offstage2",
+    "offstage3",
+    "offstage4",
+    "offstage5",
+    "offstage6",
+    "offstage7",
+    "offstage8",
+    "offstage9",
+    "offstage10",
+    "offstage11",
+    "offstage12",
+    "offstage13",
+    "offstage14",
+    "offstage15",
+    "offstage16",
+    "offstage17",
+    "offstage18",
+    "offstage19",
+    "offstage20",
+    "offstage21",
+    "offstage22",
+    "offstage23",
+    "offstage24",
+    "offstage25",
+    "offstage26",
+    "offstage27",
+    "offstage28",
+    "offstage29",
+    "offstage30",
+    "offstage31",
+    "offstage32",
+    "offstage33",
+    "offstage34",
+    "offstage35",
+    "offstage36",
+    "offstage37",
+    "offstage38",
+    "offstage39",
+    "offstage40",
+    "offstage41",
+    "offstage42",
+    "offstage43",
+    "offstage44",
+    "offstage45",
+    "offstage46",
+    "offstage47",
+    "offstage48",
+    "offstage49",
+    "offstage50",
     "groupstage1",
     "groupstage2",
     "groupstage3",
     "groupstage4",
-
     "groupoffstage1",
     "groupoffstage2",
     "groupoffstage3",
@@ -105,7 +103,8 @@ function ProgramDetail() {
         const candidates = Data.filter((candidate) => {
           return programFields.some((fieldToCheck) => {
             return (
-              candidate[fieldToCheck] === programValue && candidate.category === item.category
+              candidate[fieldToCheck] === programValue &&
+              candidate.category === item.category
             );
           });
         }).map((candidate) => ({
@@ -120,7 +119,7 @@ function ProgramDetail() {
         });
       }
     });
-  
+
     return result;
   }, []);
 
@@ -148,6 +147,7 @@ function ProgramDetail() {
     const programSlug = program.slice(0, 2);
     return categorySlug + programSlug;
   }
+
   const programData = uniqueValues.find((item) => item.slug === slug);
 
   if (programData) {
@@ -156,7 +156,7 @@ function ProgramDetail() {
       return a.darsplace.localeCompare(b.darsplace);
     });
   }
-  
+
   if (!programData) {
     // Handle the case where the program is not found
     return <div>Program not found</div>;
@@ -166,7 +166,9 @@ function ProgramDetail() {
     <div className="p-20 print:p-2 lg:flex lg:flex-col lg:items-center">
       <div className="text-center">
         <p className="text-3xl font-bold ">Maerika2k24 </p>
-        <p className="text-2xl font-bold my-2">Jawharathul Uloom Suffa Dars Art Fest 2k24</p>
+        <p className="text-2xl font-bold my-2">
+          Jawharathul Uloom Suffa Dars Art Fest 2k24
+        </p>
         <h1 className="font-bold mt-2 border-y-2 mb-4 border-black">
           {programData.program} ({programData.category})
         </h1>
